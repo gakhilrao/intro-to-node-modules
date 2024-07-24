ef8ac5e24d63eb4ec5970d63b002aa602aa11b44
gakhilrao <gullaakhilrao@gmail.com>
2024-07-24 14:48:50 +0530

    Add files via upload

---
+++ Kalki.js
@@ -0,0 +1,9 @@
+let movieTitle = "Kalki 2898 AD";
+let actors = ["Prabhas","Amitabh Bachan","Kamal Hasan","Deepika Padukone","Disha Patani"];
+
+let aboutKalkiMovie = ()=> {
+    console.log("This story is about birth of Kalki");
+
+};
+
+export {movieTitle,actors,aboutKalkiMovie};
---
+++ server.js
@@ -0,0 +1,7 @@
+import { movieTitle,actors,aboutKalkiMovie } from "./Kalki.js";
+
+
+console.log(movieTitle);
+console.log(actors);
+aboutKalkiMovie();
+
